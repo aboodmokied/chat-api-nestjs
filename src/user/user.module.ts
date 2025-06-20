8673685c66005f5e459acc528bd81c6709414c4b
AboodMokied <78992620+AboodMokied@users.noreply.github.com>
2025-06-21 02:10:59 +0300

    add welcome message

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,13 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/User';
 import { AuthModule } from 'src/auth/auth.module';
+import { ChatModule } from 'src/chat/chat.module';
 
 @Module({
   imports:[
     AuthModule,MongooseModule.forFeature([{name:User.name,schema:UserSchema}]),
     forwardRef(() => AuthModule),
+    ChatModule
   ],
   controllers: [UserController],
   providers: [UserService],
